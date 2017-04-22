88a638a16ed7749c901631f50f15a8b03d170ef9
robert5066 <robert@gmail.com>
2017-04-22 11:02:11 +0800

    a

---
+++ Base/CommonUtils/APPUtils.h
@@ -0,0 +1,13 @@
+//
+//  APPUtils.h
+//  TBBaseApp
+//
+//  Created by toby on 22/04/2017.
+//  Copyright © 2017 toby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APPUtils : NSObject
+
+@end
