453c4fe416f9c023ea9ee64ce3961f0bd4cd9da2
robert5066 <robert@gmail.com>
2017-04-22 09:09:43 +0800

    a

---
+++ Base/TBPredicate/TBPredicate.h
@@ -0,0 +1,13 @@
+//
+//  TBPredicate.h
+//  TBBaseApp
+//
+//  Created by toby on 22/04/2017.
+//  Copyright © 2017 toby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TBPredicate : NSObject
+
+@end
---
+++ Base/TBPredicate/TBPredicate.m
@@ -0,0 +1,13 @@
+//
+//  TBPredicate.m
+//  TBBaseApp
+//
+//  Created by toby on 22/04/2017.
+//  Copyright © 2017 toby. All rights reserved.
+//
+
+#import "TBPredicate.h"
+
+@implementation TBPredicate
+
+@end
