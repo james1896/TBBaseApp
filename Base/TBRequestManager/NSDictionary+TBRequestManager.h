b07a72d3dc0688e10e8efd86d13da0afbd0527a0
robert5066 <robert@gmail.com>
2017-05-07 14:05:20 +0800

    a

---
+++ Base/TBRequestManager/NSDictionary+TBRequestManager.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+TBRequestManager.h
+//  TBBaseApp
+//
+//  Created by toby on 07/05/2017.
+//  Copyright © 2017 toby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (TBRequestManager)
+
+@end
---
+++ Base/TBRequestManager/NSDictionary+TBRequestManager.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+TBRequestManager.m
+//  TBBaseApp
+//
+//  Created by toby on 07/05/2017.
+//  Copyright © 2017 toby. All rights reserved.
+//
+
+#import "NSDictionary+TBRequestManager.h"
+
+@implementation NSDictionary (TBRequestManager)
+
+@end
